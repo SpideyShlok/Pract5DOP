a9330a3456bbb2a1b006d18a9a4f0ec6ee361749
Shlokee <shlokdjhawar33@gmail.com>
2026-03-22 19:13:28 +0530

    inital commit

---
+++ src/main/java/com/mycompany/practdop/Practdop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practdop;
+
+/**
+ *
+ * @author Shlok
+ */
+public class Practdop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
